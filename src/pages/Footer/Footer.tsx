b26544560aecbd5148e5e0921c6808e4ec33dfd8
kuba <j.mieczkowski00@gmail.com>
2023-08-03 14:23:58 +0200

    adding footer

---
+++ src/pages/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <>
+      <p className="text-white">
+        <small>&copy; Copyright {new Date().getFullYear()}, xGnarly</small>
+      </p>
+    </>
+  );
+}

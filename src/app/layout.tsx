3d0a5d27a93c2bc333392b881b125d21343ba06c
kuba <j.mieczkowski00@gmail.com>
2023-08-21 12:03:21 +0200

    main layout and page changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,48 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Montserrat } from 'next/font/google'
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
 
-const montserrat = Montserrat({
-  weight: ['400', '700'],
-  subsets: ['latin'],
-})
+import "./globals.css";
+import { siteConfig } from "@/config/site";
+import Logo from "@/components/ui/Logo";
+import Footer from "@/components/ui/Footer";
+import Navigation from "@/components/ui/Navigation";
+
 
 export const metadata: Metadata = {
-  title: 'Gnarlybeatz',
+  //metadataBase: new URL(env.NEXT_PUBLIC_APP_URL),
+  title: {
+    default: siteConfig.name,
+    template: `%s | ${siteConfig.name}`,
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+});
+
+interface RootLayoutProps {
+  children: React.ReactNode;
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en" className="!scroll-smooth">
-      <body className={`${montserrat.className}`}>{children}</body>
+    <html
+      lang="en"
+      className="!scroll-smooth antialiased"
+      suppressHydrationWarning
+    >
+      <head />
+      <body
+        className={`flex flex-col min-h-screen antialiased ${montserrat.className}`}
+      >
+        <Logo />
+        <div className="container max-w-7xl mx-auto h-full">{children}</div>
+        <Navigation />
+        <Footer />
+      </body>
     </html>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,9 @@
-import Navigation from "@/pages/Navigation/Navigation";
-import Main from "@/pages/Main/Main";
-import Footer from "@/pages/Footer/Footer";
+import Dashboard from "./dashboard/page";
 
 export default function Home() {
   return (
     <>
-      <main className="flex flex-col min-h-screen items-center">
-        <Main></Main>
-      </main>
-      <footer className="flex justify-center">
-        <Footer></Footer>
-      </footer>
-      <div className="fixed inset-x-0 bottom-6 flex items-center justify-center ">
-        <Navigation />
-      </div>
+      <Dashboard />
     </>
   );
 }

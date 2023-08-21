0a6cf4dc420fc7fe4b0ff0be7f007999ae4d1a13
kuba <j.mieczkowski00@gmail.com>
2023-08-21 12:05:18 +0200

    deleted files

--- src/pages/Footer/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <>
-      <p className="text-white">
-        <small>&copy; Copyright {new Date().getFullYear()}, xGnarly</small>
-      </p>
-    </>
-  );
-}

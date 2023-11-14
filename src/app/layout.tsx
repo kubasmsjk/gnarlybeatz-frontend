7252e567987ae54e2d907850e3e2cc133200a4dc
kuba <j.mieczkowski00@gmail.com>
2023-11-14 12:57:45 +0100

    adding Toast and Providers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,9 @@ import { siteConfig } from "@/config/site";
 import Logo from "@/components/ui/Logo";
 import Footer from "@/components/ui/Footer";
 import Navigation from "@/components/ui/Navigation";
-
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Providers from "@/util/Providers";
 
 export const metadata: Metadata = {
   //metadataBase: new URL(env.NEXT_PUBLIC_APP_URL),
@@ -38,10 +40,18 @@ export default function RootLayout({ children }: RootLayoutProps) {
       <body
         className={`flex flex-col min-h-screen antialiased ${montserrat.className}`}
       >
-        <Logo />
-        <div className="container max-w-7xl mx-auto h-full">{children}</div>
+        <div id="main-logo">
+          <Logo />
+        </div>
+        <div className="container max-w-7xl h-full mx-auto">
+          <Providers>{children}</Providers>
+        </div>
         <Navigation />
         <Footer />
+        <ToastContainer
+          closeButton={false}
+          toastStyle={{ borderRadius: "10px" }}
+        />
       </body>
     </html>
   );

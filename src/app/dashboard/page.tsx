fcee9c34ba8f0b022b0b1b327870dc27a8f1647e
kuba <j.mieczkowski00@gmail.com>
2023-12-04 23:33:49 +0100

    remove style

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,11 @@
 import ContactForm from "@/components/forms/ContactForm";
-import LicencesInfoContainer from "@/components/forms/LicencesInfoContainer";
+import LicencesInfoContainer from "@/components/ui/licences/LicencesInfoContainer";
 
 export default function Dashboard() {
   return (
     <main className="container flex flex-col max-w-full items-center justify-center">
       <LicencesInfoContainer />
-      <div className="container flex flex-col max-w-full items-center justify-center">
-        <ContactForm />
-        <div className="container flex flex-col max-w-full items-center justify-center bg-black w-[20.5rem] sm:w-[30rem] h-[2.8rem] sm:h-14  mt-auto rounded-t-3xl border-t-2 border-[#8A0303]"></div>
-      </div>
+      <ContactForm />
     </main>
   );
 }

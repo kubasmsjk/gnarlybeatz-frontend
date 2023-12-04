a4761061e04549ebd0352320bec91b91c0b623e8
kuba <j.mieczkowski00@gmail.com>
2023-12-04 23:36:07 +0100

    style footer

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -1,11 +1,14 @@
 export default function Footer() {
   return (
-    <footer className="mt-auto bg-black">
-      <div className="container mx-auto max-w-full flex justify-center items-center">
-        <p>
-          <small>&copy; Copyright {new Date().getFullYear()}, xGnarly</small>
-        </p>
-      </div>
-    </footer>
+    <div className="mt-auto">
+      <div className="bg-black w-[20.5rem] sm:w-[30rem] h-[2.8rem] sm:h-14 mt-auto mx-auto rounded-t-3xl border-t-2 border-[#8A0303]"></div>
+      <footer className="mt-auto bg-black">
+        <div className="container mx-auto max-w-full flex justify-center items-center">
+          <p>
+            <small>&copy; Copyright {new Date().getFullYear()}, xGnarly</small>
+          </p>
+        </div>
+      </footer>
+    </div>
   );
 }

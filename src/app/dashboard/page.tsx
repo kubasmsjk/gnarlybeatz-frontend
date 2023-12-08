9a392f4cff0851186aa834d44f6d392fd55a4412
kuba <j.mieczkowski00@gmail.com>
2023-12-08 14:05:33 +0100

    import name changes

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,10 @@
 import ContactForm from "@/components/forms/ContactForm";
-import LicencesInfoContainer from "@/components/ui/licences/LicencesInfoContainer";
+import LicensesInfoContainer from "@/components/ui/licenses/LicensesInfoContainer";
 
 export default function Dashboard() {
   return (
     <main className="container flex flex-col max-w-full items-center justify-center">
-      <LicencesInfoContainer />
+      <LicensesInfoContainer />
       <ContactForm />
     </main>
   );

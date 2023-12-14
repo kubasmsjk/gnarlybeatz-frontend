5aa3ee6efacb409f7f10b0272220dce62e5c6bff
kuba <j.mieczkowski00@gmail.com>
2023-12-14 14:24:30 +0100

    add containers to Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Dashboard from "./dashboard/page";
+import LicensesInfoContainer from "@/app/licenses/LicensesInfoContainer";
+import ContactForm from "@/app/contact/ContactForm";
 
 export default function Home() {
   return (
-    <>
-      <Dashboard />
-    </>
+    <main className="container flex flex-col max-w-full justify-center items-center">
+      <LicensesInfoContainer />
+      <ContactForm />
+    </main>
   );
 }

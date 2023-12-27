a27a438c656863dbdadb21ef8f5f0d31301d3a48
kuba <j.mieczkowski00@gmail.com>
2023-12-27 13:10:10 +0100

    refactore code adding useFetchAudioFilesData hook

--- src/app/beats/audio/BeatsList.tsx
+++ src/app/beats/audio/BeatsList.tsx
@@ -1,88 +1,15 @@
 "use client";
 
-import axios from "axios";
-import { useInfiniteQuery } from "@tanstack/react-query";
 import { Icons } from "../../../components/ui/Icons";
-import { backendConfig } from "@/config/site";
-import { b64toBlob } from "@/app/services/b64ToBlob";
 import { useEffect, useRef } from "react";
 import { useIntersection } from "@mantine/hooks";
+import { useFetchAudioFilesData } from "@/app/services/hooks/useFetchAudioFilesData";
 import AudioPlayer from "./AudioPlayer";
-import { useBeatsFormFilter } from "@/app/hooks/useBeatsFormFilter";
-
-type AudioFileData = {
-  content: [Content];
-  pageNo: number;
-  pageSize: number;
-  totalElements: number;
-  totalPages: number;
-  last: boolean;
-};
-
-type Content = {
-  name: string;
-  genre: string;
-  mood: string;
-  bpm: string;
-  key: string;
-  audioBlob: string;
-  imageBlob: string;
-};
 
 export default function BeatsList() {
   const lastAudioFileDataRef = useRef<HTMLDivElement>(null);
-  const {
-    filterValues,
-    addSelectBpmValues,
-    addSelectKeyValues,
-    addSelectMoodsValues,
-    addSelectGenresValues,
-  } = useBeatsFormFilter();
-
-  const fetchAudioFilesData = async (page: number): Promise<AudioFileData> =>
-    await axios
-      .get(
-        backendConfig.url +
-          `/api/audio/search?pageNo=${page}&pageSize=2&name=${
-            filterValues.get("search") || ""
-          }&genre=${filterValues.get("genre") || ""}&mood=${
-            filterValues.get("mood") || ""
-          }&bpm=${filterValues.get("bpm") || ""}&key=${
-            filterValues.get("key") || ""
-          }`
-      )
-      .then((response) => {
-        response.data.content.map((element: Content) => {
-          addSelectBpmValues(element.bpm);
-          addSelectKeyValues(element.key);
-          addSelectMoodsValues(element.mood);
-          addSelectGenresValues(element.genre);
-          const audioBlob = b64toBlob(element.audioBlob, "audio/mp3");
-          const audioUrl = URL.createObjectURL(audioBlob);
-          const imageBlob = b64toBlob(element.imageBlob, "image/png");
-          const imageUrl = URL.createObjectURL(imageBlob);
-          element.audioBlob = audioUrl;
-          element.imageBlob = imageUrl;
-        });
-        return response.data;
-      });
-
-  const {
-    data: audioData,
-    fetchNextPage,
-    isError,
-    isLoading,
-  } = useInfiniteQuery({
-    queryKey: ["audioData"],
-    queryFn: async ({ pageParam = 0 }) => {
-      const response = await fetchAudioFilesData(pageParam);
-      return response;
-    },
-    getNextPageParam: (_, pages) => {
-      return pages.length;
-    },
-    initialPageParam: 0,
-  });
+  const { audioData, fetchNextPage, isError, isLoading } =
+    useFetchAudioFilesData();
 
   const _audioData = audioData?.pages.flatMap((page) => page.content);
 

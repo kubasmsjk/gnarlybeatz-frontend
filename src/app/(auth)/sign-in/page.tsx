58a5b8820489defdc7ba93a5ce7e395d51b58ce0
kuba <j.mieczkowski00@gmail.com>
2023-12-27 13:08:28 +0100

    changes sing-in page

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,21 +1,18 @@
 import Link from "next/link";
-import SignInForm from "@/components/forms/SignInForm";
-import { Icons } from "@/components/ui/Icons";
-import ApiLogin from "@/app/(auth)/user/ApiLogin";
+import SignInForm from "@/app/(auth)/sign-in/SignInForm";
 
-export default function SingIn() {
+export default function SignIn() {
   return (
     <div className="container flex flex-col max-w-full items-center justify-center">
-      <h1 className="flex justify-center py-4 sm:py-4 text-red-700">
-        <Icons.userProfile />
+      <h1 className="flex justify-center pb-7 pt-14 font-XXIIUltimateBlackMetal text-red-700 text-6xl sm:text-7xl">
+        Sign in
       </h1>
       <SignInForm />
-      <ApiLogin />
-      <p className="p-3 text-center text-sm">
+      <p className="p-3 text-center text-xs sm:text-sm">
         Don&apos;t have an account yet?{" "}
         <Link
           href={"/sign-up"}
-          className="hover:text-[#8A0303] text-sm underline underline-offset-4"
+          className="hover:text-[#8A0303] underline underline-offset-4"
         >
           Sign up
         </Link>

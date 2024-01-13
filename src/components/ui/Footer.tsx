46f7592a0c962308c54109778ea8830a0df571c9
kuba <j.mieczkowski00@gmail.com>
2024-01-13 20:45:32 +0100

    check is session user in footer css

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -1,7 +1,21 @@
+"use client";
+import { useSession } from "next-auth/react";
+
 export default function Footer() {
+  const { data: session, status } = useSession();
   return (
     <div className="mt-auto">
-      <div className="bg-black w-[20.5rem] sm:w-[30rem] h-[2.8rem] sm:h-14 mt-auto mx-auto rounded-t-3xl border-t-2 border-[#8A0303]"></div>
+      <div
+        className={
+          "bg-black h-[2.8rem] sm:h-14 mt-auto mx-auto rounded-t-3xl border-t-2 border-[#8A0303] " +
+          `${
+            session && session?.user && status === "authenticated"
+              ? "w-[19.2rem] sm:w-[27.8rem]"
+              : "w-[20.5rem] sm:w-[30rem]"
+          }`
+        }
+      ></div>
+
       <footer className="mt-auto bg-black">
         <div className="container mx-auto max-w-full flex justify-center items-center">
           <p>

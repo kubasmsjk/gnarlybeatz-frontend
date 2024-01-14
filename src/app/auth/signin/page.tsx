075a8e231ff58b4d46004620da2c78781d01fec7
kuba <j.mieczkowski00@gmail.com>
2024-01-14 14:38:16 +0100

    signin, signup session check

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,27 +1,36 @@
-"use client"
+"use client";
 
 import Link from "next/link";
 import SignInForm from "@/app/auth/signin/SignInForm";
 import { useAuthenticationForm } from "@/app/services/hooks/useAuthenticationForm";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 export default function SignIn() {
   const { formErrors } = useAuthenticationForm();
-  return (
-    <div className="container flex flex-col max-w-full items-center justify-center">
-      <h1 className="flex justify-center pb-7 pt-14 font-XXIIUltimateBlackMetal text-red-700 text-6xl sm:text-7xl">
-        Sign in
-      </h1>
-      <SignInForm />
-      <p className="p-3 text-center text-xs sm:text-sm">
-        Don&apos;t have an account yet?{" "}
-        <Link
-          href={"/auth/signup"}
-          className="hover:text-[#8A0303] underline underline-offset-4"
-          onClick={() => formErrors.clear()}
-        >
-          Sign up
-        </Link>
-      </p>
-    </div>
-  );
+  const { data: session, status } = useSession();
+  const router = useRouter();
+  if (session && session.user && status === "authenticated") {
+    router.push("/");
+  }
+  if (!session && status === "unauthenticated") {
+    return (
+      <div className="container flex flex-col max-w-full items-center justify-center">
+        <h1 className="flex justify-center pb-7 pt-14 font-XXIIUltimateBlackMetal text-red-700 text-6xl sm:text-7xl">
+          Sign in
+        </h1>
+        <SignInForm />
+        <p className="p-3 text-center text-xs sm:text-sm">
+          Don&apos;t have an account yet?{" "}
+          <Link
+            href={"/auth/signup"}
+            className="hover:text-[#8A0303] underline underline-offset-4"
+            onClick={() => formErrors.clear()}
+          >
+            Sign up
+          </Link>
+        </p>
+      </div>
+    );
+  }
 }
--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,27 +1,36 @@
-"use client"
+"use client";
 
 import Link from "next/link";
 import SignUpForm from "@/app/auth/signup/SignUpForm";
 import { useAuthenticationForm } from "@/app/services/hooks/useAuthenticationForm";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 export default function SignUp() {
   const { formErrors } = useAuthenticationForm();
-  return (
-    <div className="container flex flex-col max-w-full items-center justify-center">
-      <h1 className="flex justify-center pb-7 pt-14 font-XXIIUltimateBlackMetal text-red-700 text-6xl sm:text-7xl">
-        Sign up
-      </h1>
-      <SignUpForm />
-      <p className="p-3 text-center text-xs sm:text-sm">
-        Do you already have an account?{" "}
-        <Link
-          href={"/auth/signin"}
-          className="hover:text-[#8A0303] underline underline-offset-4"
-          onClick={() => formErrors.clear()}
-        >
-          Sign in
-        </Link>
-      </p>
-    </div>
-  );
+  const { data: session, status } = useSession();
+  const router = useRouter();
+  if (session && session.user && status === "authenticated") {
+    router.push("/");
+  }
+  if (!session && status === "unauthenticated") {
+    return (
+      <div className="container flex flex-col max-w-full items-center justify-center">
+        <h1 className="flex justify-center pb-7 pt-14 font-XXIIUltimateBlackMetal text-red-700 text-6xl sm:text-7xl">
+          Sign up
+        </h1>
+        <SignUpForm />
+        <p className="p-3 text-center text-xs sm:text-sm">
+          Do you already have an account?{" "}
+          <Link
+            href={"/auth/signin"}
+            className="hover:text-[#8A0303] underline underline-offset-4"
+            onClick={() => formErrors.clear()}
+          >
+            Sign in
+          </Link>
+        </p>
+      </div>
+    );
+  }
 }

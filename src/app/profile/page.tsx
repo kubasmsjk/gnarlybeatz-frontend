61d871195a9e8fdca5aa6eea7879d6319df946b6
kuba <j.mieczkowski00@gmail.com>
2024-01-19 23:05:04 +0100

    profile page add responsive

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -6,15 +6,15 @@ const Profile = async () => {
   const session = await getServerSession(authOptions);
   const user = session?.user;
   return (
-    <div className="flex flex-row justify-center">
-      <div className="w-1/2 flex flex-col items-center px-2 sm:px-4 border-r border-[#8A0303]">
-        <h1 className="pb-7 pt-14 font-XXIIUltimateBlackMetal text-red-700 text-4xl sm:text-5xl md:text-7xl">
+    <div className="flex flex-col lg:flex-row justify-center">
+      <div className="lg:w-1/2 flex flex-col items-center px-2 sm:px-4 border-b border-[#8A0303] lg:border-b-0 lg:border-r ">
+        <h1 className="pb-7 pt-12 md:pt-14 font-XXIIUltimateBlackMetal text-red-700 text-4xl sm:text-5xl md:text-7xl">
           Account
         </h1>
         <Account username={user?.name} email={user?.email} />
       </div>
-      <div className="h-screen"></div>
-      <div className="w-1/2 flex flex-col items-center px-2 sm:px-4">
+      <div className="lg:h-screen"></div>
+      <div className="lg:w-1/2 flex flex-col items-center px-2 sm:px-4">
         <h1 className="pb-7 pt-14 font-XXIIUltimateBlackMetal text-red-700 text-4xl sm:text-5xl md:text-7xl">
           Favorite beats
         </h1>

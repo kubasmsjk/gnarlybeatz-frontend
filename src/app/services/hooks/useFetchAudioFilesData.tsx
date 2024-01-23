1c4cd4e22569df17d0fcf38f36144a89249027c9
kuba <j.mieczkowski00@gmail.com>
2024-01-23 14:16:41 +0100

    api changes

--- src/app/services/hooks/useFetchAudioFilesData.tsx
+++ src/app/services/hooks/useFetchAudioFilesData.tsx
@@ -3,6 +3,7 @@ import { useBeatsFormFilter } from "./useBeatsFormFilter";
 import { b64toBlob } from "../b64ToBlob";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import axios from "axios";
+import { Session } from "next-auth/core/types";
 
 type AudioFileData = {
   content: [Content];
@@ -23,20 +24,25 @@ type Content = {
   imageBlob: string;
 };
 
-export const useFetchAudioFilesData = () => {
+export const useFetchAudioFilesData = (
+  pathname: string,
+  session: Session | null
+) => {
   const { filterValues } = useBeatsFormFilter();
+  const path = pathname.substring(pathname.lastIndexOf("/") + 1);
 
   const fetchAudioFilesData = async (page: number): Promise<AudioFileData> =>
     await axios
       .get(
         backendConfig.url +
-          `/api/audio/search?pageNo=${page}&pageSize=2&name=${
-            filterValues.get("search") || ""
-          }&genre=${filterValues.get("genre") || ""}&mood=${
-            filterValues.get("mood") || ""
-          }&bpm=${filterValues.get("bpm") || ""}&key=${
-            filterValues.get("key") || ""
-          }`
+          `/api/audio/search?pageNo=${page}&pageSize=2&pathname=${path}&userId=${
+            session ? Number(session?.user.id) : ""
+          }
+          &name=${filterValues.get("search") || ""}&genre=${
+            filterValues.get("genre") || ""
+          }&mood=${filterValues.get("mood") || ""}&bpm=${
+            filterValues.get("bpm") || ""
+          }&key=${filterValues.get("key") || ""}`
       )
       .then((response) => {
         response.data.content.map((element: Content) => {

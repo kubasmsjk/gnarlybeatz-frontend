055e215048812ab8fa76e18c7bd44be8b43ad1bd
kuba <j.mieczkowski00@gmail.com>
2024-01-23 14:17:57 +0100

    visual changes

--- src/app/beats/audio/AudioPlayer.tsx
+++ src/app/beats/audio/AudioPlayer.tsx
@@ -1,5 +1,11 @@
+"use client";
+
 import Image from "next/image";
 import Wavesurfer from "./Wavesurfer";
+import { Icons } from "@/components/ui/Icons";
+import { useFavoriteBeats } from "@/app/services/hooks/useFavoriteBeats";
+import { useState } from "react";
+import { useSession } from "next-auth/react";
 
 type AudioPlayerProps = {
   name: string;
@@ -9,9 +15,15 @@ type AudioPlayerProps = {
   keySignature: string;
   audioUrl: string;
   imageUrl: string;
+  pathname: string;
 };
 
 export default function AudioPlayer(props: AudioPlayerProps) {
+  const { addFavoriteBeatMutation, removeFavoriteBeatMutation } =
+    useFavoriteBeats();
+  const [buttonAnimate, setButtonAnimate] = useState(false);
+  const { data: session } = useSession();
+
   return (
     <div className="container flex flex-row w-[20.6rem] sm:w-[60rem] bg-[#8A0303] bg-opacity-20 rounded-lg shadow-lg shadow-[#660000]">
       <div className="relative w-[17%]">
@@ -26,10 +38,20 @@ export default function AudioPlayer(props: AudioPlayerProps) {
       </div>
       <div className="flex flex-col w-[83%] h-full px-1 sm:px-2 pt-1 sm:pt-2">
         <div className="flex flex-row justify-between h-fit">
-          <div className="text-xs sm:text-xl lg:text-2xl font-light pl-0.5 sm:pl-1">
+          <div
+            className={`text-xs sm:text-xl ${
+              props.pathname === "/beats" ? "lg:text-2xl" : "lg:text-xl"
+            }  font-light pl-0.5 sm:pl-1`}
+          >
             <p>{props.name}</p>
           </div>
-          <div className="flex flex-row space-x-0.5 items-center text-[0.42rem] sm:text-[0.7rem] lg:text-[0.8rem] font-light">
+          <div
+            className={`flex flex-row space-x-0.5 items-center text-[0.42rem] sm:text-[0.7rem] ${
+              props.pathname === "/beats"
+                ? "lg:text-[0.8rem]"
+                : "lg:text-[0.7rem]"
+            }  font-light`}
+          >
             <p className="bg-black rounded-md px-2 py-0.5">#{props.genre}</p>
             <p className="bg-black rounded-md px-2 py-0.5">#{props.mood}</p>
             <p className="bg-black rounded-md px-2 py-0.5">bpm: {props.bpm}</p>
@@ -40,10 +62,57 @@ export default function AudioPlayer(props: AudioPlayerProps) {
         </div>
         <div className="flex flex-row h-full">
           <div className="flex w-full h-full">
-            <Wavesurfer url={props.audioUrl} />
+            <Wavesurfer url={props.audioUrl} pathname={props.pathname} />
           </div>
-          <div className="flex justify-center items-center w-1/5 pt-1.5 sm:pt-1 lg:pt-4">
-            <button className="bg-black text-[0.4rem] sm:text-xs lg:text-sm px-0.5 md:px-3 py-0.5 sm:py-2 rounded-md">
+          <div
+            className={`flex justify-center items-center w-1/5 pt-1.5 sm:pt-1 ${
+              props.pathname === "/beats" ? "lg:pt-4" : "lg:pt-0"
+            }`}
+          >
+            {session ? (
+              <button
+                className={`w-1/3 h-3 sm:h-6 md:h-7 ${
+                  props.pathname === "/beats"
+                    ? "lg:h-9 lg:p-1"
+                    : "lg:h-5 lg:p-0"
+                } p-0 sm:p-0.5 
+                ${buttonAnimate ? "animate-jump" : ""}`}
+                onClick={() => {
+                  setButtonAnimate(!buttonAnimate);
+                  if (props.pathname === "/beats") {
+                    addFavoriteBeatMutation.mutate({
+                      userId: session.user.id,
+                      name: props.name,
+                    });
+                  } else {
+                    removeFavoriteBeatMutation.mutate({
+                      userId: session.user.id,
+                      name: props.name,
+                    });
+                  }
+                }}
+              >
+                {props.pathname === "/beats" ? (
+                  <Icons.heart />
+                ) : (
+                  <Icons.heartOff />
+                )}
+              </button>
+            ) : null}
+            <button
+              className={`bg-black rounded sm:rounded-md
+              ${
+                session && session?.user
+                  ? `w-2/3 text-[0.3rem] sm:text-[0.6rem] sm:leading-[1rem] ${
+                      props.pathname === "/beats"
+                        ? "lg:text-xs"
+                        : "lg:text-[0.6rem]"
+                    } px-[0.06rem] md:px-1 lg:px-2 py-0.5 md:py-1.5 ${
+                      props.pathname === "/beats" ? "lg:py-2" : "lg:py-1"
+                    }`
+                  : `text-[0.4rem] sm:text-xs lg:text-xs px-0.5 md:px-2 py-0.5 sm:py-2`
+              }`}
+            >
               Add to cart
             </button>
           </div>
--- src/app/beats/audio/BeatsList.tsx
+++ src/app/beats/audio/BeatsList.tsx
@@ -5,11 +5,19 @@ import { useEffect, useRef } from "react";
 import { useIntersection } from "@mantine/hooks";
 import { useFetchAudioFilesData } from "@/app/services/hooks/useFetchAudioFilesData";
 import AudioPlayer from "./AudioPlayer";
+import { usePathname } from "next/navigation";
+import { useSession } from "next-auth/react";
+import { useFavoriteBeats } from "@/app/services/hooks/useFavoriteBeats";
 
 export default function BeatsList() {
+  const { data: session, status } = useSession();
+  const { favoriteBeatErrors } = useFavoriteBeats();
+  const pathname = usePathname();
+
   const lastAudioFileDataRef = useRef<HTMLDivElement>(null);
+
   const { audioData, fetchNextPage, isError, isLoading } =
-    useFetchAudioFilesData();
+    useFetchAudioFilesData(pathname, session);
 
   const _audioData = audioData?.pages.flatMap((page) => page.content);
 
@@ -25,26 +33,59 @@ export default function BeatsList() {
   }, [entry, fetchNextPage]);
 
   return (
-    <div className="container max-w-full flex flex-col justify-center items-center">
-      {isLoading && (
-        <div role="status" className="flex justify-center items-center h-96">
-          <Icons.loading />
-          <span className="sr-only">Loading...</span>
-        </div>
-      )}
-      {isError && (
-        <div className="flex flex-col justify-center items-center h-96">
-          <Icons.serverError />
-          <h1 className="text-1xl sm:text-2xl">Server error</h1>
-        </div>
-      )}
-      {_audioData?.map((element, index) => {
-        if (index === _audioData.length - 1)
+    <>
+      {favoriteBeatErrors.get("account") != "" ? (
+        <span className="flex justify-center w-[95%] pb-8 text-xs sm:text-sm text-red-600">
+          {favoriteBeatErrors.get("account")}
+        </span>
+      ) : null}
+      {favoriteBeatErrors.get("file") != "" ? (
+        <span className="flex justify-center w-[95%] pb-8 text-xs sm:text-sm text-red-600">
+          {favoriteBeatErrors.get("file")}
+        </span>
+      ) : null}
+
+      <div className="container max-w-full flex flex-col justify-center items-center">
+        {isLoading && (
+          <div role="status" className="flex justify-center items-center h-96">
+            <Icons.loading />
+            <span className="sr-only">Loading...</span>
+          </div>
+        )}
+        {isError && (
+          <div className="flex flex-col justify-center items-center h-96">
+            <Icons.serverError />
+            <h1 className="text-1xl sm:text-2xl">Server error</h1>
+          </div>
+        )}
+        {_audioData?.map((element, index) => {
+          if (index === _audioData.length - 1)
+            return (
+              <div
+                className={`container flex justify-center h-[4.1rem] sm:h-[7rem] md:h-[8.85rem] ${
+                  pathname === "/beats" ? "lg:h-[11rem]" : "lg:h-[7rem]"
+                } p-1.5 lg:p-2.5 animate-fade-up animate-delay-500`}
+                key={index}
+                ref={ref}
+              >
+                <AudioPlayer
+                  name={element.name}
+                  genre={element.genre}
+                  mood={element.mood}
+                  bpm={element.bpm}
+                  keySignature={element.key}
+                  audioUrl={element.audioBlob}
+                  imageUrl={element.imageBlob}
+                  pathname={pathname}
+                />
+              </div>
+            );
           return (
             <div
-              className="container flex justify-center h-[4.1rem] sm:h-[7rem] md:h-[8.85rem] lg:h-[11rem] p-1.5 lg:p-2.5 animate-fade-up animate-delay-500"
+              className={`container flex justify-center h-[4.1rem] sm:h-[7rem] md:h-[8.85rem] ${
+                pathname === "/beats" ? "lg:h-[11rem]" : "lg:h-[7rem]"
+              } p-1.5 lg:p-2.5 animate-fade-up animate-delay-500`}
               key={index}
-              ref={ref}
             >
               <AudioPlayer
                 name={element.name}
@@ -54,26 +95,12 @@ export default function BeatsList() {
                 keySignature={element.key}
                 audioUrl={element.audioBlob}
                 imageUrl={element.imageBlob}
+                pathname={pathname}
               />
             </div>
           );
-        return (
-          <div
-            className="container flex justify-center h-[4.1rem] sm:h-[7rem] md:h-[8.85rem] lg:h-[11rem] p-1.5 lg:p-2.5 animate-fade-up animate-delay-500"
-            key={index}
-          >
-            <AudioPlayer
-              name={element.name}
-              genre={element.genre}
-              mood={element.mood}
-              bpm={element.bpm}
-              keySignature={element.key}
-              audioUrl={element.audioBlob}
-              imageUrl={element.imageBlob}
-            />
-          </div>
-        );
-      })}
-    </div>
+        })}
+      </div>
+    </>
   );
 }
